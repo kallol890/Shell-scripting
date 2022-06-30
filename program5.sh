7c8cc2db995d71d4636808890bf4e96d7727c010
Kallol Mahato1 <kallol.mahato1@ibm.com>
2022-06-30 19:44:14 +0530

    Weekdays

---
+++ program5.sh
@@ -0,0 +1,30 @@
+echo " Enter a no"
+read num
+case $num in
+1)
+echo "Today is Monday"
+;;
+2)
+echo "Today is Tuesday"
+;;
+3)
+echo "Today is Wednesday"
+;;
+4)
+echo "Today is Thursday"
+;;
+5)
+echo "Today is Friday"
+;;
+6)
+echo "Today is Saturday"
+;;
+7)
+echo "Today is Sunday"
+;;
+
+*)
+echo "Invalid input"
+
+esac
+
